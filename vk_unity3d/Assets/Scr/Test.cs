0ff8c81287f9d99296e3805fa6efe8b6ef770161
Natalya Gulyaeva <n.gulyaeva@iz.ru>
2019-02-28 12:01:32 +0300

    сцена с вызовом функций unity <--> html javascript

---
+++ vk_unity3d/Assets/Scr/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Text _text;
+
+    public void MyFunction(string arg)
+    {
+        _text.text = arg;
+    }
+
+    public void Click()
+    {
+        Application.ExternalCall("SayHello", "The game says hello!");
+    }
+}
